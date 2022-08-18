778b14a758aaf4d396d4fdf0dded4b15732f247b
Anton_Baranov <ab@blotch.ru>
2022-08-18 07:58:09 +0300

    add random

--- SUM/Program.cs
+++ SUM/Program.cs
@@ -1,3 +1,5 @@
-﻿int number_1 = 3;
-int number_2 = 5;
+﻿int number_1 = new Random().Next(1, 10);
+Console.WriteLine(number_1);
+int number_2 = new Random().Next(1, 10);
+Console.WriteLine(number_2);
 Console.WriteLine(number_1 + number_2);
